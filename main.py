bb6bd7b9e89b754e8df7ad2f2d8701ff02526c99
personcoz <249801150+personcoz@users.noreply.github.com>
2025-12-15 18:38:43 +0000

    ui but its not gonna ever be used

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-# Test @sullra @personcoz
-#add banch
+import time
+
+cookie = ""
+PlaceID = False
+isPrivateServer = False
+privateserverID = ""
+
+def do_Inputs():
+    PlaceId = str(input("Please input the place id of the game!"))
+
+    cookie = str(input("Please input your roblox cookie (leave blank to auto-detect)"))
+
+    checkForPrivate = str(input("Is there a private server? (Y/N)"))
+
+    while checkForPrivate.lower() != "y" or checkForPrivate.lower() != "n":
+        print(checkForPrivate.lower(), checkForPrivate.lower() != "n")
+        time.sleep(0.01)
+        checkForPrivate = str(input("It appears you did a typo, please type 'Y' or 'N' to indicate if there is a private server?"))
+    
+    if checkForPrivate.lower == "y":
+        isPrivateServer = True
+        privateserverID = "?privateServerLinkCode=" + str(input("Please input the bit after the '?privateServerLinkCode=' e.g: '8236306244555044u7280d0238655898' Note: if this is wrong this could break the script"))
+
+
+do_Inputs()
